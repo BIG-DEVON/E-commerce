2356924a4432b251d3361368635efefe071ec5bb
Devon <ezeajie14@gmail.com>
2022-06-17 01:06:33 +0100

    ..

--- e-commerce/components/HeroBanner.jsx
+++ e-commerce/components/HeroBanner.jsx
@@ -8,20 +8,20 @@ const HeroBanner = ({ heroBanner }) => {
     <div className='hero-banner-container'> 
     <div>
         <p className='beats-solo'>{heroBanner.smallText}</p>
-        <h3> MID TEXT </h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <img src=' ' alt='headphones'
+        <h3> {heroBanner.midText} </h3>
+        <h1> {heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt='headphones'
         className="hero-banner-image "/>
 
         <div>
-           <Link href="/product/ID">
-            <button type="button">  BUTTON TEXT
+           <Link href={`/product/${heroBanner.product}`}>
+            <button type="button"> {heroBanner.buttonText}
             </button>
 
             </Link> 
          <div className='desc'> 
          <h5> Description</h5>
-         <p> DESCRIPTION</p>
+         <p>{heroBanner.desc}</p>
          </div>
         </div>
       </div>
